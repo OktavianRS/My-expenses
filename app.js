30f718b7f5c6dee25d6f61771c47546051c6dcb6
OktavianRS <edarli@mail.ru>
2016-02-04 19:15:17 +0100

    Added simple controller redirect from / to /home

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes');
+  , app = express.createServer()
+  , pages = require(__dirname + '/controllers/pages') 
 
-var app = module.exports = express.createServer();
 
-// Configuration
+// mount routes
+app.get('/', function (req, res) { res.redirect('/home') })
+app.get('/home', pages.home)
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+module.exports = app
